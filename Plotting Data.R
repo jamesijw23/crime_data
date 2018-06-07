3d542a91adeabda56a3bf00f6522080df7736fab
Immanuel Williams <jamesijw23@gmail.com>
2018-06-06 22:25:41 -0700

    Color to ggplot

--- Plotting Data.R
+++ Plotting Data.R
@@ -11,7 +11,10 @@ gathered_ki = gather(kill_injured,type,number,-state)
 gathered_ki$type = ifelse(gathered_ki$type =="i_state","Injured","Killed")
 
 ggplot(gathered_ki, aes(x=state,y=number,fill = type)) + 
-   geom_bar(stat = "identity", position=position_dodge()) +
+  geom_bar(stat = "identity", position=position_dodge()) +
   xlab("Top 10 States") +
   ylab("Count") + 
-  ggtitle("")
+  ggtitle("Top Murders by State") + 
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5))  + 
+  scale_fill_manual("legend", values = c("Injured" = "black", "Killed" = "red"))
